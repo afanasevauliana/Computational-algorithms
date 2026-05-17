52fb18474e4d4120fe7ba69aa24bd561a3635a8e
afanasevauliana <afanasevauliana946@gmail.com>
2026-05-18 00:13:22 +0300

    l

--- lab4/task1.py
+++ lab4/task1.py
@@ -1,8 +1,22 @@
+def merger_method(arr):
+    if len(arr) <= 1:
+        return arr
+    sr = len(arr) // 2
+    left_side = merger_method(arr[:sr])
+    right_side = merger_method(arr[sr:])
+
+    
+
 with open("text_for_lab4.txt", 'r', encoding='utf-8') as file:
     a = file.readline().split()
-    cnt_rows = a[0]
-    cnt_cols = a[1]
-    content = file.read()
-    print(cnt_rows, cnt_cols)
-    print(content)
+    cnt_rows = int(a[0])
+    cnt_cols = int(a[1])
 
+    flat_list = []
+    for i in range(cnt_rows):
+        line = file.readline().split()
+        for k in line:
+            flat_list.append(int(k))
+    print(f"Количество элементов: {cnt_rows * cnt_cols}")
+    print(f"Одномерный массив: {flat_list}")
+    merger_method(flat_list)
