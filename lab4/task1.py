4d6a1ba897c2bc021882acf0e7db0b2349807e96
afanasevauliana <afanasevauliana946@gmail.com>
2026-05-18 17:19:18 +0300

    l

--- lab4/task1.py
+++ lab4/task1.py
@@ -1,22 +1,33 @@
-def merger_method(arr):
+def merge_sort(arr):
     if len(arr) <= 1:
         return arr
     sr = len(arr) // 2
-    left_side = merger_method(arr[:sr])
-    right_side = merger_method(arr[sr:])
+    left_side = merge_sort(arr[:sr])
+    right_side = merge_sort(arr[sr:])
+    return merge_list(left_side, right_side)
+def merge_list(a, b):
+    c = []
+    i = j = 0
+    while i < len(a) and j < len(b):
+        if a[i] <= b[j]:
+            c.append(a[i])
+            i += 1
+        else:
+            c.append(b[j])
+            j += 1
+    return c + a[i:] + b[j:]
 
-    
-
-with open("text_for_lab4.txt", 'r', encoding='utf-8') as file:
+with open("m1.txt", 'r', encoding='utf-8') as file:
     a = file.readline().split()
     cnt_rows = int(a[0])
     cnt_cols = int(a[1])
 
-    flat_list = []
+    array = []
     for i in range(cnt_rows):
         line = file.readline().split()
         for k in line:
-            flat_list.append(int(k))
+            array.append(int(k))
     print(f"Количество элементов: {cnt_rows * cnt_cols}")
-    print(f"Одномерный массив: {flat_list}")
-    merger_method(flat_list)
+    print(f"Одномерный массив: {array}")
+    print(f"Отсортированный массив: {merge_sort(array)}")
+
